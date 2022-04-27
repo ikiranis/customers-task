7e267b8b48c52f0afdc2a650c5512a589ee94335
rocean <rocean74@gmail.com>
2022-04-28 02:55:12 +0300

    Migrations

--- database/migrations/2022_04_27_224329_create_payments_table.php
+++ database/migrations/2022_04_27_224329_create_payments_table.php
@@ -13,14 +13,16 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('payments', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('customer_email', 30)->unique();
+            $table->integer('amount')->unsigned();
+            $table->date('date');
             $table->timestamps();
+            $table->foreign('customer_email')
+                ->references('email')
+                ->on('customers')
+                ->onDelete('cascade');
         });
     }
 
@@ -31,6 +33,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('payments');
     }
 };
