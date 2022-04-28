5977eb0831e6c49ac965d9765bf4250ead0d46cf
rocean <rocean74@gmail.com>
2022-04-29 01:31:40 +0300

    Payments button

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -19,6 +19,7 @@
                         <th scope="col">Id</th>
                         <th scope="col">Name</th>
                         <th scope="col">Email</th>
+                        <th scope="col" class="text-center">Actions</th>
                     </tr>
                     </thead>
                     <tbody>
@@ -28,16 +29,25 @@
                             <td><a href="{{route('customers.edit', $customer->id)}}">{{$customer->id}}</a></td>
                             <td>{{ $customer->name }}</td>
                             <td>{{ $customer->email }}</td>
-                            <td>
-                                <form method="POST" action="{{route('customers.destroy', $customer->id)}}">
-                                    <input name="_method" type="hidden" value="DELETE">
-                                    @csrf
+                            <td class="row">
+                                <div class="col text-center">
+                                    <a href="{{route('customers.show', $customer->id)}}">
+                                        <button type="submit" class="btn btn-sm btn-info">Payments</button>
+                                    </a>
+                                </div>
 
-                                    <button type="submit" class="btn btn-danger"
-                                            onclick="return confirm('Είσαι σίγουρος για την διαγραφή;')">
-                                        Delete
-                                    </button>
-                                </form>
+                                <div class="col text-center">
+                                    <form method="POST" action="{{route('customers.destroy', $customer->id)}}">
+                                        <input name="_method" type="hidden" value="DELETE">
+                                        @csrf
+
+                                        <button type="submit" class="btn btn-sm btn-danger"
+                                                onclick="return confirm('Are you sure to delete the record;')">
+                                            Delete
+                                        </button>
+                                    </form>
+                                </div>
+
                             </td>
                         </tr>
                     @endforeach
@@ -48,7 +58,7 @@
 
                 <div class="row mt-5 col-4 mx-auto">
                     <a href="{{route('importPayments')}}">
-                        <button class="btn btn-success w-100">Import CSV file</button>
+                        <button class="btn btn-warning w-100">Import CSV file</button>
                     </a>
                 </div>
             @else
