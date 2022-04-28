3214636ca09f09527215ad9b49e6bf12e0103850
rocean <rocean74@gmail.com>
2022-04-28 05:44:53 +0300

    Customer controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CustomerController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [CustomerController::class, 'index'])->name('home');
