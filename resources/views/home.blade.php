9986b42d890f2c2a9524ad879c9402261c31405f
rocean <rocean74@gmail.com>
2022-04-28 15:05:14 +0300

    Remove bootstrap installation

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,11 +6,8 @@
 
     <title>Customers</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
 </head>
 <body>
